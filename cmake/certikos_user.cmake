b281bf19f79188ef467d98f22bcadbc0484160bc
Richard Habeeb <richard.habeeb@gmail.com>
2023-09-20 20:24:53 -0400

    new certikos user build

--- cmake/certikos_user.cmake
+++ cmake/certikos_user.cmake
@@ -2,15 +2,15 @@ cmake_minimum_required(VERSION 3.13)
 
 set (HAS_TEST false)
 
-execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=libgcc.a
-    OUTPUT_VARIABLE LIBGCC
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=libm.a
-    OUTPUT_VARIABLE LIBM
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-link_libraries(${LIBGCC} ${LIBM})
+#execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=libgcc.a
+#    OUTPUT_VARIABLE LIBGCC
+#    OUTPUT_STRIP_TRAILING_WHITESPACE)
+#
+#execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=libm.a
+#    OUTPUT_VARIABLE LIBM
+#    OUTPUT_STRIP_TRAILING_WHITESPACE)
+#
+#link_libraries(${LIBGCC} ${LIBM})
 
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_options(-O0 -g)
